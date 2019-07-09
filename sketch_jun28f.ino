1fed630ff25c1367d0019e47fac807c512d7ce59
avo2003 <50109997+avo2003@users.noreply.github.com>
2019-07-10 01:49:23 +0400

    Add files via upload

---
+++ sketch_jun28f.ino
@@ -0,0 +1,12 @@
+void setup(){
+  Serial.begin(9600);
+ } 
+ void loop(){
+  int array[10];
+        for(int i = 0; i< 10;i++){
+          array[i] = random(50);
+          Serial.println(array[i]);
+          delay(100);
+          }
+          
+     }
